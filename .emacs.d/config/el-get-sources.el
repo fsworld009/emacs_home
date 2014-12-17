557544f7bc482b4113b21e9c03a56b9ac40ccb14
WorldFS <fsworld009@gmail.com>
2014-12-17 08:24:58 -0500

    evil-mode and modes build on evil-mode

--- .emacs.d/config/el-get-sources.el
+++ .emacs.d/config/el-get-sources.el
@@ -1,2 +1,15 @@
+(setq my:el-get-packages '())
 
+;; theme
 (add-to-list 'my:el-get-packages 'smyx-theme)
+
+;;evil
+(add-to-list 'my:el-get-packages 'evil)
+(add-to-list 'my:el-get-packages 'evil-leader)
+(add-to-list 'my:el-get-packages 'ace-jump-mode)
+(add-to-list 'my:el-get-packages 'evil-nerd-commenter)
+(add-to-list 'my:el-get-packages 'evil-matchit)
+(add-to-list 'my:el-get-packages 'evil-surround)
+
+;;common modes
+
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 (add-to-list 'exec-path "C:/Program Files (x86)/git/bin/")
 
 ;;el-get
-(setq my:el-get-packages '())
+
 (load "~/.emacs.d/config/el-get-sources")
 (load "~/.emacs.d/config/el-get_init")
 
